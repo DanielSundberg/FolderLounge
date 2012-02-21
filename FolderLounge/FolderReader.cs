4f9c28b562aa1acd5ba6e9bdd93240f709c6a8ef
Daniel Sundberg <s>
2012-02-21 21:17:23 +0100

    Read recent links without throwing exceptions on invalid files

--- FolderLounge/FolderReader.cs
+++ FolderLounge/FolderReader.cs
@@ -10,17 +10,18 @@ namespace FolderLounge
     {
         public static string GetLnkTarget(string lnkPath)
         {
-            var shl = new Shell32.Shell();         // Move this to class scope
-            lnkPath = System.IO.Path.GetFullPath(lnkPath);
-            var dir = shl.NameSpace(System.IO.Path.GetDirectoryName(lnkPath));
-            var itm = dir.Items().Item(System.IO.Path.GetFileName(lnkPath));
-            try
+            FileInfo fileInfo = new FileInfo(lnkPath);
+            if (fileInfo.Extension.ToUpper() == ".LNK")
             {
-                // Remove exception
+                var shl = new Shell32.Shell();         // Move this to class scope
+                lnkPath = System.IO.Path.GetFullPath(lnkPath);
+                var dir = shl.NameSpace(System.IO.Path.GetDirectoryName(lnkPath));
+                var itm = dir.Items().Item(System.IO.Path.GetFileName(lnkPath));
+
                 var lnk = (Shell32.ShellLinkObject)itm.GetLink;
                 return lnk.Target.Path;
             }
-            catch (NotImplementedException)
+            else
             {
                 return string.Empty;
             }
